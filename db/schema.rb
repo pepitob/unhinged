7585150f45961d57c36568fc85b5ae7583a5c526
Sarah Mitko <sarahmitko@gmail.com>
2022-11-21 15:43:35 +0100

    adding sign up and new model user with devise

--- db/schema.rb
+++ db/schema.rb
@@ -42,11 +42,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_21_132302) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "places"
