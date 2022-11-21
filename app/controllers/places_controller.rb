09fec9558885a6f8de0b51bc35b05c12bac1df29
Sarah Mitko <sarahmitko@gmail.com>
2022-11-21 17:42:11 +0100

    created delete method in controller and added to index view

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -16,6 +16,12 @@ class PlacesController < ApplicationController
     end
   end
 
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
+    redirect_to places_path
+  end
+
   private
 
   def list_params
