9ef06ba5c18a0fa3da4888b2899f563ac9e45f69
Sofia CachoRuschner <sofiamcr@gmail.com>
2022-11-22 14:13:21 +0100

    adding newand create for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,38 @@
 class BookingsController < ApplicationController
+  before_action :set_place, only: %i[new create]
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    if current_user
+      @booking = Booking.new
+    else
+      redirect_to new_user_session_path
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.place = @place
+    @nights = (@booking.end_date - @booking.start_date).to_i
+    @booking.total_price = @booking.place.price * @nights
+    if @booking.save
+      redirect_to place_bookings_path(@booking.place.id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :number_of_people, :total_price)
+  end
+
+  def set_place
+    @place = Place.find(params[:place_id])
+  end
+
 end
