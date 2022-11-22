cba00f1f174e3af058402a92816417f0a301d312
Sofia CachoRuschner <sofiamcr@gmail.com>
2022-11-22 14:46:35 +0100

    adding validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,7 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validate :end_date_after_start_date
+  validate :range_date
 
   private
 
@@ -15,4 +16,14 @@ class Booking < ApplicationRecord
     end
   end
 
+  def range_date
+    date_ranges = Booking.all.map { |b| b.start_date..b.end_date }
+    date_ranges.each do |range|
+      if range.include? start_date
+        return errors.add(:start_date, "Already booked for this date")
+      elsif range.include? end_date
+        return errors.add(:end_date, "Already booked for this date")
+      end
+    end
+  end
 end
