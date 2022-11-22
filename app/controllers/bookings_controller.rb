a109ee5377f6f221661b1e6ab0d5298c286dd6fe
root <hippolytebourban@gmail.com>
2022-11-22 12:59:29 +0100

    added edit/create to bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,31 @@
 class BookingsController < ApplicationController
+  before_action :set_place, only: %i[new create edit update]
   def index
     @bookings = Booking.all
   end
+
+  def edit
+    @booking = Booking.find(params[:id])
+  end
+
+  def update
+    @booking.place = @place
+    @nights = (@booking.end_date - @booking.start_date).to_i
+    @booking.total_price = @nights * price
+    if @booking.update(booking_params)
+      redirect_to booking_path
+    else
+      render :new
+    end
+  end
+end
+
+private
+
+def booking_params
+  params.require(:booking).permit(:start_date, :end_date, :number_of_people, :total_price)
+end
+
+def set_place
+  @place = Place.find(params[:place_id])
 end
