14e0334d9413fb1640f7c63fa3c8ec8025b0b0ce
root <hippolytebourban@gmail.com>
2022-11-23 21:16:24 +0100

    display map on index page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,13 @@
 class PlacesController < ApplicationController
   def index
     @places = Place.all
+
+    @markers = @places.geocoded.map do |place|
+      {
+        lat: place.latitude,
+        lng: place.longitude
+      }
+    end
   end
 
   def new
