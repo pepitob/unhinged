1c74318a27702455ce6f04af94fdc5c35dae5d70
Hippolyte Bourban <hippolytebourban@gmail.com>
2023-01-04 21:59:20 +0100

    fixed autotyping when clicking on search bar

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -3,14 +3,16 @@ import Typed from "typed.js"
 
 
 export default class extends Controller {
-
   connect() {
-    // const audio = new Audio('https://www.computerhope.com/jargon/m/example.mp3');
-    // audio.play();
-    new Typed(this.element, {
+    this.typed = new Typed(this.element, {
       strings: ["Somewhere epic to get wasted in Barcelona", "Not a lame ass party place", "Millionaire boat parties", "private island to get shit housed with my friends"],
       typeSpeed: 30,
       loop: true
-    })
+    });
+  }
+  stopTyping = () => {
+    this.typed.stop();
+    this.typed.reset();
+    this.element.value = "";
   }
 }
